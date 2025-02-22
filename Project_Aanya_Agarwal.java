a3d650b22e6c9b9f12dc2dc35fd6a2aa9befd168
Aanya Agarwal <agaraanya@gmail.com>
2025-02-22 12:12:41 -0500

    Update Project_Aanya_Agarwal.java
    implicitly called toString

--- Project_Aanya_Agarwal.java
+++ Project_Aanya_Agarwal.java
@@ -53,14 +53,7 @@ public class Project_Aanya_Agarwal{
       for(Policy policy : policyList)
       { 
          //display information about the Policy
-         System.out.println("Policy Number: " + policy.getPolicyNumber());
-         System.out.println("Provider Name: " + policy.getProviderName());
-         System.out.println("Policyholder's First Name: " + policy.getFirstName());
-         System.out.println("Policyholder's Last Name: " + policy.getLastName());
-         System.out.println("Policyholder's Age: " + policy.getAge());
-         System.out.println("Policyholder's Smoking Status: " + policy.getSmokingStatus());
-         System.out.println("Policyholder's Height: " + policy.getHeight() + " inches");
-         System.out.println("Policyholder's Weight: " + policy.getWeight() + " pounds");
+         System.out.println(policy);
          System.out.printf("Policyholder's BMI: %.2f\n", policy.getBMI());
          System.out.printf("Policy Price: $%.2f\n", policy.getPrice());
          System.out.println();
@@ -68,6 +61,9 @@ public class Project_Aanya_Agarwal{
          if(policy.getSmokingStatus().equalsIgnoreCase("smoker"))//keep track of the number of smokers
             numSmokers++;
       }
+      
+      //print out number of policies created
+      System.out.printf("There were %d Policy objects created.\n", Policy.numPolicies);
       
       //print out the number of smokers and non-smokers
       System.out.println("The number of policies with a smoker is: " + numSmokers);
